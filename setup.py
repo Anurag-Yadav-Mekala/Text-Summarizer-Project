69774e04db63c9ffce5c234f348f87020e553639
Anurag-Yadav-Mekala <anuragyadavmekala@gmail.com>
2025-06-21 00:23:32 +0530

    requirements & setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "Anurag_Yadav"
+AUTHOR_USER_NAME = "Anurag-Yadav-Mekala"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "anuragyadavmekala@gmail.com"
 
@@ -18,13 +18,49 @@ setuptools.setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small python package for NLP app",
+    description="A small Python package for an NLP-based text summarization app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",  # <-- corrected field name
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Source":     f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+    install_requires=[
+        "transformers",
+        "transformers[sentencepiece]",
+        "datasets",
+        "sacrebleu",
+        "rouge_score",
+        "py7zr",
+        "pandas",
+        "nltk",
+        "tqdm",
+        "PyYAML",
+        "matplotlib",
+        "torch",
+        "notebook",
+        "boto3",
+        "mypy-boto3-s3",
+        "python-box==6.0.2",
+        "ensure==1.0.2",
+        "fastapi==0.78.0",
+        "uvicorn==0.18.3",
+        "Jinja2==3.1.2",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
 )
